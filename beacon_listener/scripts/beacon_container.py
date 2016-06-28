1aea73dde8953d5db3c4570aa729ef3e23847a62
Jakub Szlendak <szlendak.jakub@gmail.com>
2016-06-28 21:07:18 +0200

    Make BeaconContainer iterable

--- beacon_listener/scripts/beacon_container.py
+++ beacon_listener/scripts/beacon_container.py
@@ -10,17 +10,32 @@ class BeaconContainer:
         self.__lock = Lock()
         self.__max_age = max_age
 
+    def __iter__(self):
+        with self.__lock:
+            for k, v in self.__beacons.items():
+                yield v
+
     def clean(self):
+        """Cleans up container, deleting items older than max_age attibute"""
         with self.__lock:
             self.__beacons = {addr: dev for addr, dev in self.__beacons.items() if
                               time.time() - dev.updated_at < self.__max_age}
 
     def insert(self, addr, dev):
+        """Insert/update of device. Thread safe"""
         with self.__lock:
             self.__beacons[addr] = dev
 
     def get(self, addr):
+        """Returns device of given address"""
         if addr in self.__beacons:
             return self.__beacons[addr]
         else:
             return None
+
+
+
+    def dump(self):
+        """Dumps device list"""
+        for k, v in self.__beacons.items():
+            print k, v
--- beacon_listener/scripts/test/beaconContainer.py
+++ beacon_listener/scripts/test/beaconContainer.py
@@ -22,3 +22,11 @@ class TestBeaconContainer(TestCase):
         container.clean()
         self.assertIsNone(container.get('7e:96:b0:dc:fc:95'))
         self.assertIsNotNone(container.get('7e:96:b0:dc:fc:96'))
+
+    def test_iter(self):
+        container = BeaconContainer()
+        container.insert(packet.addr, packet)
+        container.insert(packet2.addr, packet2)
+        for a, b in container:
+            self.assertIsNotNone(a)
+            self.assertIsNotNone(b)
