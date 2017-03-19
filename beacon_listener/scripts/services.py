a21965e48a63261f607a53cf7bed808aab742284
Jakub Szlendak <szlendak.jakub@gmail.com>
2017-03-19 23:26:19 +0100

    Add launchfile and parameter server cfg, additional service for retrieving location tags

--- beacon_listener/scripts/services.py
+++ beacon_listener/scripts/services.py
@@ -1,20 +1,20 @@
 import rospy
 from binascii import hexlify
 
-from beacon_msgs.srv import GetAvailableDevices, GetAvailableDevicesResponse
-from beacon_msgs.msg import Advertising
+from beacon_msgs.srv import GetAvailableBeaconsResponse, GetAvailableLocationTagsResponse
+from beacon_msgs.msg import Advertising, LocationTag
 
 
-class AvaliableDevicesServiceWrapper:
-    """Encapsulates avaliable_devices service
+class AvailableBeaconsServiceWrapper:
+    """Encapsulates available_devices service
         This service returns list of devices available to connect with"""
 
     def __init__(self, container):
         self.__container = container
 
     def handler(self, req):
-        req = GetAvailableDevicesResponse()
-        rospy.logdebug("Called avaliable_devices service")
+        req = GetAvailableBeaconsResponse()
+        rospy.loginfo("Called available_devices service")
 
         for beacon in self.__container:
             req.beacons.append(self.beacon_to_ros_msg(beacon))
@@ -27,4 +27,25 @@ class AvaliableDevicesServiceWrapper:
                            updated_at=rospy.Time.from_sec(beacon.updated_at),
                            rssi=beacon.rssi,
                            data=hexlify(beacon.raw))
+
+
+class AvailableLocationTagsServiceWrapper:
+    """Encapsulates available_devices service
+        This service returns list of devices available to connect with"""
+
+    def __init__(self, container, filter):
+        self.__container = container
+        self.__filter = filter
+
+    def handler(self, req):
+        req = GetAvailableLocationTagsResponse()
+        rospy.loginfo("Called available_devices service")
+
+        filtered_list = self.__filter.do_filter(self.__container)
+
+        for beacon in filtered_list:
+            req.tags.append(beacon)
+
+        return req
+
 
--- beacon_listener/src/beacon_listener.py
+++ beacon_listener/src/beacon_listener.py
@@ -10,15 +10,9 @@ from scripts.publisher import PublishingWrapper
 from scripts.beacon_localization import BeaconLocalizationFilter
 from scripts.services import *
 from beacon_msgs.msg import LocationTag
+from beacon_msgs.srv import GetAvailableBeacons, GetAvailableLocationTags
 from bluepy.btle import Scanner
 import rospy
-
-# move to parameter server:
-scan_time = 1
-beacon_max_age = 3
-publish_rate = 1
-group_id = 0xAAAA
-manufacturer_id = 0xffff
 
 
 def shutdown_hook():
@@ -32,28 +26,38 @@ if __name__ == "__main__":
     rospy.loginfo("Starting Beacon Listener")
     rospy.on_shutdown(shutdown_hook)
 
+    SCAN_TIME = rospy.get_param('beacon_listener/scan_time')
+    MAX_BEACON_AGE = rospy.get_param('beacon_listener/max_beacon_age')
+    PUBLISH_RATE = rospy.get_param('beacon_listener/publish_rate')
+    GROUP_ID = rospy.get_param('beacon_listener/group_id')
+
     # create scanner
-    beacon_container = Container(beacon_max_age)
+    beacon_container = Container(MAX_BEACON_AGE)
     scan_delegate = BeaconScanDelegate(container=beacon_container)
     scanner = Scanner().withDelegate(scan_delegate)
-    scan_worker = ScanWorker(scanner, scan_time=scan_time)
+    scan_worker = ScanWorker(scanner, scan_time=SCAN_TIME)
 
     # start scanning
     scan_worker.start()
     rospy.loginfo("Scanning...")
 
     # init filtering
-    adv_filter = BeaconLocalizationFilter(group_id, beacon_max_age)
+    adv_filter = BeaconLocalizationFilter(GROUP_ID, MAX_BEACON_AGE)
     ros_pub = rospy.Publisher('/beacon_localization/location_tag', LocationTag, queue_size=100)
     publisher = PublishingWrapper(adv_filter=adv_filter, container=beacon_container, pub=ros_pub)
 
     # init services
-    available_devs_svc_wrapper = AvaliableDevicesServiceWrapper(beacon_container)
-    available_devs_svc = rospy.Service('/beacon_localization/get_available_devices', GetAvailableDevices, available_devs_svc_wrapper.handler)
-    rospy.loginfo("'/beacon_localization/get_available_devices' service started")
+    available_beacons_svc_wrapper = AvailableBeaconsServiceWrapper(beacon_container)
+    available_beacons_svc = rospy.Service('/beacon_localization/get_all_available_beacons', GetAvailableBeacons, available_beacons_svc_wrapper.handler)
+    rospy.loginfo("/beacon_localization/get_all_available_beacons service started")
 
-    r = rospy.Rate(1 / publish_rate)
+    available_tags_svc_wrapper = AvailableLocationTagsServiceWrapper(beacon_container, adv_filter)
+    available_tags_svc = rospy.Service('/beacon_localization/get_available_location_tags', GetAvailableLocationTags, available_tags_svc_wrapper.handler)
+    rospy.loginfo("/beacon_localization/get_available_location_tags' service started")
+
+    r = rospy.Rate(PUBLISH_RATE)
     while not rospy.is_shutdown():
         beacon_container.clean()
         publisher.publish()
         r.sleep()
+
