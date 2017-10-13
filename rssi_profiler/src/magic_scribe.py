8984e62e1abac573ab5e71770bb1c536c0c4a155
Jakub Szlendak <szlendak.jakub@gmail.com>
2017-10-13 16:13:25 +0200

    Resolve #4 : robot frame can be parametrized in MAGICAL scribe

--- rssi_profiler/src/magic_scribe.py
+++ rssi_profiler/src/magic_scribe.py
@@ -8,46 +8,51 @@ from beacon_msgs.srv import GetBeaconDistances
 import numpy as np
 import rospy
 import tf
+from argparse import ArgumentParser
 
 
 def main():
     # init ros
     rospy.init_node("rssi_scribe")
 
-    args = rospy.myargv(argv=sys.argv)
-
-    if len(args) != 1:
-        print "Usage:"
-        print "python magic_scribe.py [output filename]"
-        exit(1)
+    argparser = ArgumentParser()
+    argparser.add_argument('--base',
+                           help='BLE sensor base frame, default: /batman/base_link',
+                           default='/batman/base_link')
+    argparser.add_argument('--filter',
+                           help='RSSI filter to be used: probabilistic(default), moving_average, recent',
+                           default='probabilistic')
+    args = argparser.parse_args(rospy.myargv()[1:])
 
     rospy.loginfo("Starting !!!MAGICAL!!! RSSI Scribe")
-    rospy.loginfo("If encountering problems with transform, make sure  base link and beacons TF are published and sychronized (refer to https://answers.ros.org/question/123256/sync-ros-bag-timestamps-with-ros-system/ if using rosbag")
+    rospy.loginfo(
+        "If encountering problems with transform, make sure  base link and beacons TF are published and sychronized \
+         (refer to https://answers.ros.org/question/123256/sync-ros-bag-timestamps-with-ros-system/ if using rosbag")
+
+    base_frame = args.base
+    filter_type = args.filter
+    result = {}
 
     beacons = rospy.get_param('/beacon_localization/map/beacons')
-
-    result = {}
 
     rospy.sleep(0.1)
     t = tf.TransformListener()
     for beacon in beacons:
         frame = '/beacon_' + beacon['name'] + '_' + beacon['id']
-        # rssi = [response.rssi for response in distances_response.measurements if response.bid == beacon['id']][0]
-        # print rssi
-        t.waitForTransform(frame, '/batman/base_link', rospy.Time(0), rospy.Duration(10.0))
-        (trans, r) = t.lookupTransform(frame, '/batman/base_link', rospy.Time(0))
+        t.waitForTransform(frame, base_frame, rospy.Time(0), rospy.Duration(10.0))
+        (trans, r) = t.lookupTransform(frame, base_frame, rospy.Time(0))
 
         result[beacon['id']] = np.linalg.norm(trans)
 
-    rospy.wait_for_service('/beacon_localization/distances/probabilistic')
-    distances_srv = rospy.ServiceProxy('/beacon_localization/distances/probabilistic', GetBeaconDistances)
+    rospy.wait_for_service('/beacon_localization/distances/'+filter_type)
+    distances_srv = rospy.ServiceProxy('/beacon_localization/distances/'+filter_type, GetBeaconDistances)
     distances_response = distances_srv.call()
 
-    for id,dist in result.items():
+    for id, dist in result.items():
         res = [response for response in distances_response.measurements if response.bid == id][0]
         try:
-            print 'beacon_'+ res.name + '_' + res.bid + '.txt'
-            with open('beacon_'+ res.name + '_' + res.bid + '.txt', 'a') as outfile:
+            print 'beacon_' + res.name + '_' + res.bid + '.txt'
+            with open('beacon_' + res.name + '_' + res.bid + '.txt', 'a') as outfile:
 
                 line = str(dist)
                 line += '\t'
@@ -57,6 +62,7 @@ def main():
         except IOError as e:
             print 'Unable to open file: ', e
             exit(1)
+
 
 if __name__ == "__main__":
     main()
